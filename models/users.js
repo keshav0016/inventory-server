0368868db7e04a139619411c91cabba58abcc107
keshav0016 <keshav.b@westagilelabs.com>
2018-03-28 12:05:07 +0530

    server

--- models/users.js
+++ models/users.js
@@ -2,12 +2,15 @@
 const argon2 = require('argon2')
 module.exports = (sequelize, DataTypes) => {
   var users = sequelize.define('users', {
-    user_id: {type:DataTypes.INTEGER, unique:true, min:5, required:true},
-    password: {type:DataTypes.STRING, min:5, max:10, required:true},
-    name:{type:DataTypes.STRING},
+    password: {type:DataTypes.STRING, validate: {len: {args: [5,10]} }, required:true, },
+    first_name:{type:DataTypes.STRING,  allowNull: false, validate: {is: ["^[a-z]+$",'i'] }},
+    last_name: {type:DataTypes.STRING,  allowNull: false, validate: {is: ["^[a-z]+$",'i'] }},
+    age: {type:DataTypes.INTEGER,  allowNull: false, validate: { isInt: true,}},
+    gender: {type:DataTypes.STRING,  allowNull: false,},
     role: DataTypes.STRING,
     token: DataTypes.ARRAY(DataTypes.TEXT),
-    department: DataTypes.STRING
+    department: {type:DataTypes.STRING,  allowNull: false,},
+    designation: {type:DataTypes.STRING,  allowNull: false,},
   
   },{
     classMethods: {
@@ -32,7 +35,7 @@ module.exports = (sequelize, DataTypes) => {
       users.password = hash
     })
   })
-
+ 
   users.verifyPassword = function(userSubmittedPassword, user){
     return argon2.verify(user.password, userSubmittedPassword)
   }
