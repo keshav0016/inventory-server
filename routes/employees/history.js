cb587d694114e555598104bf1fd9737d30250abe
mulakalaveena <m.veena2k14@gmail.com>
2018-04-05 22:25:45 +0530

    chnages made in employees list and employees history

--- routes/employees/history.js
+++ routes/employees/history.js
@@ -4,31 +4,26 @@ const router = require('express').Router()
 
 
 function employeeHistoryHandler(req, res, next){
-    var history = []
-    models.users.findAll({ where : {user_id : req.body.user_id}})
-    .then(employee => {
-        history.push(...employee)
-        return models.assets_assigned.findAll({ where : {user_id : req.body.user_id}})
-    })
-    .then(employeeAsset => {
-        history.push(...employeeAsset)
-        return models.consumables_assigned.findAll({ where : {user_id : req.body.user_id}})
-    })
-    .then(employeeConsumable => {
-        history.push(...employeeConsumable)
-        history.sort(function(a, b){return b.updatedAt - a.updatedAt})
-        res.json({
-            history : history
+        // var search_consumableid = req.query.consumableid
+        var history = []
+        models.consumables_assigned.findAll({ where : {user_id : req.body.user_id}})
+        .then(consumableAssign => {
+            history.push(...consumableAssign)
+            return models.assets_assigned.findAll({ where : {user_id : req.body.user_id}})
         })
-
-    })
-    .catch(error => {
-        console.log(error)
-        res.json({
-            error : error.message || "History could not be displayed"
+        .then(assetAssign => {
+            history.push(...assetAssign)
+            history.sort(function(a, b){return b.updatedAt - a.updatedAt})
+            res.json({
+                history : history
+            })
         })
-    })
-}
+        .catch(error => {
+            res.json({
+                error : error.message || "History could not be displayed"
+            })
+        })
+    }
 
 
 
