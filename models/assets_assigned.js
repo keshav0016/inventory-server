a74660981ac7a5e9994c3a1ae5d6afd8d7883ebd
EmmanuelCBaby <emmanuel.b@westagilelabs.com>
2018-04-06 10:20:02 +0530

    changed the datatype of user_id to string in asset-assigned

--- models/assets_assigned.js
+++ models/assets_assigned.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var assets_assigned = sequelize.define('assets_assigned', {
-    asset_id: DataTypes.INTEGER,
+    asset_id: DataTypes.STRING,
     user_id: DataTypes.INTEGER,
     ticket_number: DataTypes.INTEGER,
     from: DataTypes.DATE,
