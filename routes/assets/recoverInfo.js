b35d0e90a703d5d13e30a7035d2c73b00a4d5448
keshav0016 <keshav.b@westagilelabs.com>
2018-04-10 12:05:14 +0530

    associations of asset repair and assets

--- routes/assets/recoverInfo.js
+++ routes/assets/recoverInfo.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 
 
 function recoverInfoHandler(req, res, next){
-    models.assets_assigned.findOne({where : {asset_id : req.query.asset_id, to : null}})
+    models.assets_assigned.findOne({where : {asset_id : req.query.asset_id, to : null}, include : [{model : models.assets, where : {asset_id : req.query.asset_id}}, {model : models.users, attributes : ['user_id', 'first_name', 'last_name']}]})
     .then(assignedRecord => {
         res.json({
             recoverInfo : assignedRecord
--- routes/assets/repairInfo.js
+++ routes/assets/repairInfo.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 
 
 function repairInfoHandler(req,res,next){
-    models.assets_repair.findOne({ where: {asset_id : req.query.asset_id, to : null}})
+    models.assets_repair.findOne({ where: {asset_id : req.query.asset_id, to : null}, include : [{model : models.assets, where : {asset_id : req.query.asset_id}}]})
     .then(repairRecord => {
         res.json({
             repairInfo : repairRecord
