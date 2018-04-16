73e7adad0b7040ea2f9e11d8f7815c52f78e8f09
keshav0016 <keshav.b@westagilelabs.com>
2018-04-16 12:21:48 +0530

    updated accept assets

--- routes/ticketsAdmin/acceptAsset.js
+++ routes/ticketsAdmin/acceptAsset.js
@@ -2,19 +2,32 @@ const models = require('../../models/index')
 
 function acceptAssetTicketHandler(req, res){
     var user;
+    var assetId
+    var assetName
     models.ticket.findOne({ where: {ticket_number : req.body.ticket_number}})
     .then(ticket => {
-        ticket.status = 'Accepted'
-        ticket.reason = req.body.reason
-        user = ticket.user_id
-        return ticket.save()
-    })
-    .then(ticket => {
-        return models.assets.findOne({ where : {asset_id : ticket.requested_asset_id}})
+        assetName = ticket.requested_asset_item
+        return models.assets.findOne({ where : {asset_id : ticket.requested_asset_id, current_status : 'Available'}})
     })
     .then(asset => {
-        asset.current_status = "Assigned"
-        return asset.save()
+        if(!asset){
+            return models.assets.findOne({where : {asset_name : assetName, current_status : 'Available'}})
+        }
+        else{
+            return Promise.resolve(asset)
+        }
+    })
+    .then(asset => {
+        if(asset){
+            asset.current_status = "Assigned"
+            assetId = asset.asset_id
+            return asset.save()
+        }
+        else{
+            res.json({
+                message : 'No Asset Available'
+            })
+        }
     })
     .then(asset => {
         var newAssetAssigned = models.assets_assigned.build({
@@ -25,6 +38,16 @@ function acceptAssetTicketHandler(req, res){
             expected_recovery : req.body.expected_recovery
         })
         return newAssetAssigned.save()
+    })
+    .then(assetAssigned => {
+        return models.ticket.findOne({ where: {ticket_number : req.body.ticket_number}})
+    })
+    .then(ticket => {
+        ticket.status = 'Accepted'
+        ticket.reason = req.body.reason
+        ticket.requested_asset_id = assetId
+        user = ticket.user_id
+        return ticket.save()
     })
     .then(assetAssigned => {
         res.json({
