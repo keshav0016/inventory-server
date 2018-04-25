3bd3a2ad4f7b0781054ebec240f12fe98c7a5e13
keshav0016 <keshav.b@westagilelabs.com>
2018-04-25 14:12:13 +0530

    commented the index migration

--- migrations/20180424102523-updating-asset_id.js
+++ migrations/20180424102523-updating-asset_id.js
@@ -2,16 +2,16 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.removeConstraint(
-      'assets',
-      'assets_serial_number_key'
-    )
+    // return queryInterface.removeConstraint(
+    //   'assets',
+    //   'assets_serial_number_key'
+    // )
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.addConstraint('assets', ['serial_number'], {
-      type: 'unique',
-      name: 'assets_serial_number_key'
-    })
+    // return queryInterface.addConstraint('assets', ['serial_number'], {
+    //   type: 'unique',
+    //   name: 'assets_serial_number_key'
+    // })
   }
 };
