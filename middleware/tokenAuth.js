c565dc4065beb916c98ce7ae549396687a032395
veena devi <veenadevi@veenas-MacBook-Pro.local>
2018-05-15 15:44:39 +0530

    changes made in token middlewares

--- middleware/tokenAuth.js
+++ middleware/tokenAuth.js
@@ -5,18 +5,16 @@ const jwt= require('jsonwebtoken')
 
 
 function tokenMiddleware(req,res,next){
-    console.log('user token')
     var token=req.cookies.token;
     var decodedtoken = jwt.verify(token,'lovevolleyball');
     models.users.findOne({ where : {user_id:decodedtoken.user_id,role:'Employee', token : [token] }})
     .then(user=>{
         if(user){
             req.currentUser=user,
-            console.log(req.currentUser.user_id)
             next()
             
         }else{
-            res.send('user not found' )
+            res.send('Employee not found' )
         }
     })
     .catch(error=>{
