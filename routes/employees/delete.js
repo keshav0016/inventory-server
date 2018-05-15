4ed365837a97a42674fc8bb56ec7a9427c9f6de4
Keshav Bohra <keshavbohra@Keshavs-MacBook-Pro.local>
2018-05-15 15:00:25 +0530

    fixed the employee diable

--- routes/employees/delete.js
+++ routes/employees/delete.js
@@ -2,20 +2,31 @@ const models = require('../../models/index')
 const router = require('express').Router()
 
 function disableEmployeeHandler(req, res, next){
+    let userDisable = 0;
     models.users.findOne({include: [{model : models.assets_assigned}], where : {user_id : req.body.user_id}})
     .then(user => {
         user.assets_assigneds.forEach(assets_assigned => {
             if(assets_assigned.to === null){
-                res.json({
-                    message: 'recover the assets first'
-                })
+                userDisable = 0;
+                return Promise.resolve(user)
             }else{
-                user.disable = 1;
-                return user.save()
+                userDisable = 1;
             }
         });
+        return Promise.resolve(user)        
         // user.disable = 1;
         // return user.save()
+    })
+    .then(user =>{
+        if(userDisable === 0){
+            res.json({
+                message : 'recover the assets first'
+            })
+        }
+        else{
+            user.disable = 1;
+            return user.save()
+        }
     })
     .then(user => {
         res.json({
