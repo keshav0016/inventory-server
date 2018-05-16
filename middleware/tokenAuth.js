d8126b6bae97ec7d30cbe3b831ed6572dfe2e976
Keshav Bohra <keshav.b@westagailelabs.com>
2018-05-16 17:43:13 +0530

    full

--- middleware/tokenAuth.js
+++ middleware/tokenAuth.js
@@ -6,7 +6,6 @@ const jwt= require('jsonwebtoken')
 
 function tokenMiddleware(req,res,next){
     var receivedToken=req.cookies.token;
-<<<<<<< HEAD
     if(receivedToken){
         var decodedtoken = jwt.verify(receivedToken,'lovevolleyball');
         models.users.findOne({ where : {user_id:decodedtoken.user_id,role:'Employee', token :{$contains : [receivedToken]}  }})
@@ -26,21 +25,5 @@ function tokenMiddleware(req,res,next){
     else{
         res.status(401)
     }
-=======
-    var decodedtoken = jwt.verify(receivedToken,'lovevolleyball');
-    models.users.findOne({ where : {user_id:decodedtoken.user_id,role:'Employee', token :{$contains : [receivedToken]}  }})
-    .then(user=>{
-        if(user){
-            req.currentUser=user,
-            next()
-            
-        }else{
-            res.send('Employee not found' )
-        }
-    })
-    .catch(error=>{
-        next(error)
-    })
->>>>>>> f18ca0f8c9bdc6a38899d1f6151833c05da80a8c
 }
 module.exports=exports=tokenMiddleware
