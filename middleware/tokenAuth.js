ab516bebf28f487396a39c0607ee021aa12ea997
veena devi <veenadevi@veenas-MacBook-Pro.local>
2018-05-16 17:47:05 +0530

    chnages in token auths

--- middleware/tokenAuth.js
+++ middleware/tokenAuth.js
@@ -6,27 +6,6 @@ const jwt= require('jsonwebtoken')
 
 function tokenMiddleware(req,res,next){
     var receivedToken=req.cookies.token;
-<<<<<<< HEAD
-    if(receivedToken){
-        var decodedtoken = jwt.verify(receivedToken,'lovevolleyball');
-        models.users.findOne({ where : {user_id:decodedtoken.user_id,role:'Employee', token :{$contains : [receivedToken]}  }})
-        .then(user=>{
-            if(user){
-                req.currentUser=user,
-                next()
-                
-            }else{
-                res.status(403).send('Employee not found' )
-            }
-        })
-        .catch(error=>{
-            next(error)
-        })
-    }
-    else{
-        res.status(401)
-    }
-=======
     var decodedtoken = jwt.verify(receivedToken,'lovevolleyball');
     models.users.findOne({ where : {user_id:decodedtoken.user_id,role:'Employee', token :{$contains : [receivedToken]}  }})
     .then(user=>{
@@ -41,6 +20,5 @@ function tokenMiddleware(req,res,next){
     .catch(error=>{
         next(error)
     })
->>>>>>> f18ca0f8c9bdc6a38899d1f6151833c05da80a8c
 }
 module.exports=exports=tokenMiddleware
