152eabd2d3ec18a273633d6ba741593863d7f289
Keshav Bohra <keshav.b@westagailelabs.com>
2018-05-17 09:59:10 +0530

    removed bug for the asset disabled

--- routes/ticketsAdmin/availableAssets.js
+++ routes/ticketsAdmin/availableAssets.js
@@ -5,7 +5,7 @@ const router = require('express').Router()
 function availableAssetsHandler(req, res, next){
     models.ticket.findOne({where : {ticket_number : req.query.ticket}})
     .then(ticket => {
-        return models.assets.findAll({where : {current_status : 'Available', assetType : ticket.requested_asset_item}})
+        return models.assets.findAll({where : {current_status : 'Available', assetType : ticket.requested_asset_item, disabled : {$notIn : [1]}}})
     })
     .then(assets => {
         res.json({
