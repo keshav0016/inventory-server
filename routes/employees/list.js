5d1210851a0c2d94f4850c82d1b616b6e52c6fdf
Emmanuel CB <emmanuelcb@Emmanuels-MacBook-Pro.local>
2018-05-31 16:53:30 +0530

    added the employee search feature

--- routes/employees/list.js
+++ routes/employees/list.js
@@ -1,16 +1,20 @@
 const models = require('../../models/index')
 const router = require('express').Router()
+const Sequelize = require('sequelize')
 
 function listEmployee(req, res) {
     var page = Number(req.query.page) || 1
     var department = req.query.department || "%"
     var pagination = {}
+    var nameSearch = req.query.search
+    var idSearch = req.query.search
 
     models.users.count({where:{role:'Employee', department : {like : department}}})
     .then(numberOfRecords => {
         pagination.totalPage = Math.ceil(numberOfRecords / 10);
         pagination.currentPage = page;
-        return models.users.scope('withoutPassword').findAll({where:{role:'Employee', department : {like : department}},order:[['createdAt','DESC']], limit: 10, offset: (page - 1) * 10})
+        // return models.users.scope('withoutPassword').findAll({where : {role:'Employee', department : {like : department},$or : []},order:[['createdAt','DESC']], limit: 10, offset: (page - 1) * 10})
+        return models.users.scope('withoutPassword').findAll({where : Sequelize.and({role:'Employee'}, {department : {like : department}},Sequelize.or({user_id : {like : '%'+idSearch+'%'}},Sequelize.where(Sequelize.fn('CONCAT',Sequelize.col('first_name'),' ',Sequelize.col('last_name')),{ilike : '%'+nameSearch+'%'}))),order:[['createdAt','DESC']], limit: 10, offset: (page - 1) * 10})
     })
     .then(user=> {
         res.json({user, message:'employees list is found', pagination});
