190a9e738bb53ee5b8d406a5abf0d95b72c50129
veena devi <veenadevi@veenas-MacBook-Pro.local>
2018-06-30 13:34:43 +0530

    email integration for enable and disable employee

--- routes/employees/enable.js
+++ routes/employees/enable.js
@@ -1,5 +1,8 @@
 const models = require('../../models/index')
 const router = require('express').Router()
+const api = require('../../config/sendGrid')
+const sgMail = require('@sendgrid/mail');
+
 
 function enableEmployeeHandler(req, res, next){
     models.users.findOne({ where : {user_id : req.body.user_id}})
@@ -8,6 +11,16 @@ function enableEmployeeHandler(req, res, next){
         return user.save()
     })
     .then(user => {
+        sgMail.setApiKey(api)
+        const msg = {
+            to : user.email,
+            from : 'hr@westagilelabs.com'
+            ,subject : 'Welcome to Wal Inventory management system'
+            ,html : `<p>Hello ${user.first_name},<br />This Email is to inform you that You will now be able to use the West Agile Labs's Inventory Management Tool with your previous credentials. `
+        }  
+        return sgMail.send(msg)     
+    })
+    .then(() => {
         res.json({
             message : 'Employee ennabled successfully'
         })
