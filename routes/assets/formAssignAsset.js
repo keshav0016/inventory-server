7dff61538eefd0da4993e80e281e53684fade2e8
veena devi <veenadevi@veenas-MacBook-Pro.local>
2018-07-02 21:53:34 +0530

    deploy

--- routes/assets/formAssignAsset.js
+++ routes/assets/formAssignAsset.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 function formAssignAssetHandler(req, res, next){
     var allEmployees = []
-    models.users.findAll({ where : {role : 'Employee' ,disable: 0}, attributes : ['user_id', 'first_name','last_name', 'department']})
+    models.users.findAll({ where : {role : 'Employee' ,disable: 0}, attributes : ['user_id', 'first_name','last_name', 'department'],order:[['first_name','ASC']]})
     .then(employees => {
         allEmployees.push(...employees);
         return models.assets.findAll({ where : {current_status : 'Available'}, attributes : ['asset_name', 'category'], group : ['asset_name', 'category'] })
