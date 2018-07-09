7db487034a8542fa8a1528aa0c83568ad630997b
veena devi <veenadevi@veenas-MacBook-Pro.local>
2018-07-09 18:57:11 +0530

    vendor update changes

--- routes/vendor/update.js
+++ routes/vendor/update.js
@@ -3,15 +3,61 @@ const router = require('express').Router()
 
 
 function updateVendorHandler(req, res, next){
+    let saveAllUserPromise = []
+    var prevVendor;
     models.vendor.findOne({where : {id : req.body.id}})
+    .then(vendor => {
+        prevVendor = vendor.name
+        return models.assets.findAll({where: {vendor: prevVendor}})
+        
+    })
+    .then(asset => {
+        asset.forEach(e => {
+            e.vendor = req.body.name
+            saveAllUserPromise.push(e.save())
+        })
+        return Promise.resolve(asset)
+     
+    })
+    .then(asset => {
+        return models.consumables_purchased.findAll({where: {vendor_name : prevVendor}})
+    })
+    .then(consumable => {
+        consumable.forEach(e => {
+            e.vendor_name = req.body.name
+            saveAllUserPromise.push(e.save())
+        })
+        return Promise.resolve(consumable)
+    })
+    .then(consumable => {
+        return models.assets_repair.findAll({where: {vendor : prevVendor}})
+    })
+    .then(assetRepair => {
+        assetRepair.forEach(e => {
+            e.vendor = req.body.name
+            saveAllUserPromise.push(e.save())
+
+        })
+        return Promise.resolve(assetRepair)
+    })
+    .then(assetRepair => {
+        return models.vendor.findOne({where: {id: req.body.id}})
+    })
     .then(vendor => {
         vendor.name = req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1).toLowerCase(),
         vendor.address = req.body.address
         vendor.contact = req.body.contact
         vendor.landline = req.body.landline
-        return vendor.save()
+        saveAllUserPromise.push(vendor.save())
+
+        return Promise.resolve(vendor)
+        
     })
     .then(vendor => {
+        return Promise.all(saveAllUserPromise)
+       
+    })
+    .then(() => {
         res.json({
             message: 'vendor has been updated'
         })
