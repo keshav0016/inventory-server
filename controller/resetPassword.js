b577b30b99ffb2a5349372c69444b217cec0a29a
veena devi <veenadevi@veenas-MacBook-Pro.local>
2018-07-20 12:29:20 +0530

    changes made for forgot password and admin login

--- controller/resetPassword.js
+++ controller/resetPassword.js
@@ -4,7 +4,45 @@ const router = require('express').Router()
 
 
 function resetPasswordHandler(req, res, next){
-    models.users.findOne({where : {user_id : req.body.user_id}})
+    if(req.body.user_id === 'Admin' || req.body.user_id === 'admin'){
+        models.Admin.findOne({ where : {email : req.body.email}})
+        .then(admin => {
+            if(admin){
+                return argon2.verify(admin.password, req.body.emailPassword)
+            }
+            else {
+                res.json({
+                    message : 'user not found'
+                })
+            }
+        })
+        .then(match => {
+            if(match){
+                let hashedPassword
+                return argon2.hash(req.body.password)
+                .then(hashed => {
+                    hashedPassword = hashed
+                    return models.Admin.findOne({where : {email : req.body.email}})
+                })
+                .then(admin => {
+                    admin.password = hashedPassword,
+                    admin.firstLogin = 0
+                    return admin.save()
+                })
+                .then(admin => {
+                    res.json({
+                        message : 'password has been changed'
+                    })
+                })
+            }
+        })
+        .catch(error => {
+            res.json({
+                error : error
+            })
+        })
+    }else {
+        models.users.findOne({where : {user_id : req.body.user_id}})
     .then(user => {
         if(user){
             return argon2.verify(user.password, req.body.emailPassword)            
@@ -40,6 +78,8 @@ function resetPasswordHandler(req, res, next){
             error : error
         })
     })
+    }
+    
 }
 
 
