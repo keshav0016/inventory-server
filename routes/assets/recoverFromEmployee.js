03fc676e78e63efe3bd838c4ff75e5f1262ec653
veena-wal <veena.m@westagilelabs.com>
2018-09-12 12:25:39 +0530

    recovery email

--- routes/assets/recoverFromEmployee.js
+++ routes/assets/recoverFromEmployee.js
@@ -1,10 +1,17 @@
 const models = require('../../models/index')
 const router = require('express').Router()
+const sgMail = require('@sendgrid/mail');
+const api = require('../../config/sendGrid')
 
 
 function recoverAssetHandler(req, res, next){
+    var user_id
+    var assetName
+    var assetType
     models.assets.findOne({ where : {asset_id : req.body.asset_id}})
     .then(asset => {
+        assetName = asset.asset_name
+        assetType = asset.assetType
         asset.current_status = "Available"
         asset.condition = 'Used'
         return asset.save()
@@ -13,10 +20,29 @@ function recoverAssetHandler(req, res, next){
         return models.assets_assigned.findOne({where : {asset_id : req.body.asset_id, to : null}})
     })
     .then(assetAssigned => {
+        user_id = assetAssigned.user_id
         assetAssigned.to = req.body.to
         return assetAssigned.save()
     })
     .then(assetAssigned => {
+        return models.users.findOne({
+            where : {
+                user_id : user_id
+            }
+        })
+    })
+    .then(user => {
+        sgMail.setApiKey(api)
+        const msg = {
+            to : user.email,
+            from : 'hr@westagilelabs.com'
+            ,subject : 'An Asset Recovered from you'
+        ,html : `<p>Hello ${user.first_name},<br/><br/>An Asset called <b>${assetName}</b> ${assetType} has been recovered from you. For further queries feel free to reach admin department.<br /><br />Thanks,<br />Team Admin</p>`
+        }  
+        return sgMail.send(msg)      
+
+    })
+    .then(() => {
         res.json({
             message : "Asset recovered"
         })
