1f97655bf9af8dd8a03810690e11150ce0ca2def
Emmanuel CB <emmanuel.b@westagilelabs.com>
2019-03-12 11:06:53 +0530

    changed the scheduler time.

--- scheduler.js
+++ scheduler.js
@@ -25,9 +25,9 @@ var consumableDetails = [];
 var fileId = process.env.FILE_ID
 var folderId = process.env.FOLDER_ID
 
-var sched = later.parse.recur().every(10).second(),
-    // var sched = later.parse.recur().on('11:30:00').time().onWeekday() ,
-    t = later.setInterval(itemStatusReportEmail, sched);
+// var sched = later.parse.recur().every(10).second(),
+var sched = later.parse.recur().on('11:30:00').time().onWeekday() ,
+t = later.setInterval(itemStatusReportEmail, sched);
 var limitDate = new Date(Number(new Date()))
 console.log('scheduler has started')
 
